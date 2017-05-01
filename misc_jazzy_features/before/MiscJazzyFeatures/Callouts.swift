42cd231983c0014a156780c647be9ec6c8403980
John Fairhurst <johnfairh@gmail.com>
2017-05-01 13:26:59 +0100

    Spec updates for issue 476

--- misc_jazzy_features/before/MiscJazzyFeatures/Callouts.swift
+++ misc_jazzy_features/before/MiscJazzyFeatures/Callouts.swift
@@ -10,15 +10,27 @@
 ///
 /// - Parameters:
 ///     - aParameter: A parameter
+///       Use *this* parameter like so:
+///
+///         ```swift
+///         aParameter
+///         ```
+///     - bParameter: Another parameter
 ///
 /// - Returns: A result
-public func functionA(_ aParameter: Bool) -> Bool {
+///   More description of the result
+///
+///     Some reasons for the result:
+///     1. A reason
+///     2. This reason's text has an apostrophe
+///
+public func functionA(_ aParameter: Bool, bParameter: Bool) -> Bool {
     return true
 }
 
 /// Function (alternate documentation comment style)
 ///
-/// - Parameter: aParameter: A parameter
+/// - Parameter aParameter: A parameter
 public func functionB(_ aParameter: Bool) -> Bool {
     return true
 }
@@ -37,5 +49,15 @@ public func functionC(_ aParameter: Bool) -> Bool {
 ///
 /// - parameter: aParameter: A parameter
 public func functionD(_ aParameter: Bool) -> Bool {
+    return true
+}
+
+/// Function (alternate documentation comment style #2)
+///
+/// - Parameter aParameter: A parameter
+///   More about the parameter
+///
+///     Even more about the parameter
+public func functionE(_ aParameter: Bool) -> Bool {
     return true
 }
