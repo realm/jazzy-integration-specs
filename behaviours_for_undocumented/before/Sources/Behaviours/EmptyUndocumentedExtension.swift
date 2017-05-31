5d59bea8b9055a3c3c62f5f29b1a29b80469cc2b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2017-05-30 18:41:41 -0600

    Added class external not just to module, but to module group.

--- behaviours_for_undocumented/before/Sources/Behaviours/EmptyUndocumentedExtension.swift
+++ behaviours_for_undocumented/before/Sources/Behaviours/EmptyUndocumentedExtension.swift
@@ -2,14 +2,31 @@
 import Dependency
 
 extension EmptyExtensionToExternalType_Should_be_HIDDEN {
-    /// An extension method (to ensure the extension is not empty.)
+    /// An extension method (but private, so the extension is still empty.)
     private func extensionMethod() {}
 }
 extension EmptyExtensionToExternalType_Should_be_HIDDEN where Component : Equatable {
-    /// An extension method (to ensure the extension is not empty.)
+    /// An extension method (but internal, so the extension is still empty.)
     internal func extensionMethod() {}
 }
 extension EmptyExtensionToExternalType_Should_be_HIDDEN where Component == Bool {
+    // The extension is still empty.
     /// :nodoc:
     public func extensionMethod() {}
+}
+
+import Foundation
+
+extension NSBundle {
+    /// An extension method (but private, so the extension is still empty.)
+    private func extensionMethodPrivate() {}
+}
+extension NSBundle {
+    /// An extension method (but internal, so the extension is still empty.)
+    internal func extensionMethodInternal() {}
+}
+extension NSBundle {
+    // The extension is still empty.
+    /// :nodoc:
+    public func extensionMethodOptedOut() {}
 }
